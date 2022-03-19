e4a67dc7de3b1339b9e7021d87a3d632ec5ebb35
Julien Haudegond <44610840+Julien-Haudegond@users.noreply.github.com>
2022-03-19 15:31:51 +0100

    [AfterFX] Update CLI with no-gui and script-path arguments

--- starterpack/vendors/afterfx/cli.py
+++ starterpack/vendors/afterfx/cli.py
@@ -6,10 +6,19 @@ def can_be_registered():
 
 def register_cli(parser_to_configure):
     parser_to_configure.add_argument("--bin-path", dest="bin_path", type=str, help="Path to the application binary.")
+    parser_to_configure.add_argument(
+        "--no-gui",
+        dest="no_gui",
+        action="store_true",
+        help="Run the application in batch mode (without GUI).",
+    )
+    parser_to_configure.add_argument(
+        "--script-path", dest="script_path", type=str, help="Path to an AfterFX script to execute at startup."
+    )
 
 
 def execute(args):
     from starterpack.vendors.afterfx.core.launcher import Launcher
 
-    launcher = Launcher(bin_path=args.bin_path)
+    launcher = Launcher(bin_path=args.bin_path, no_gui=args.no_gui, script_path=args.script_path)
     launcher.execute()
