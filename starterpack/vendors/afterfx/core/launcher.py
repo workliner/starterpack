7a36dddab7a1ec7821b45504ca5df7439e0308b3
Julien Haudegond <44610840+Julien-Haudegond@users.noreply.github.com>
2022-03-19 17:54:00 +0100

    [AfterFX] Launcher: add script arguments handling

--- starterpack/vendors/afterfx/core/launcher.py
+++ starterpack/vendors/afterfx/core/launcher.py
@@ -1,4 +1,5 @@
 import os
+from typing import Dict
 
 import starterpack.vendors.abstract
 from starterpack.vendors.afterfx.core.command_builder import _CommandBuilder
@@ -11,12 +12,17 @@ class Launcher(starterpack.vendors.abstract.Launcher):
         bin_path: (optional) path to the AfterFX binary.
         no_gui: (optional) run the application in batch mode. Works only when 'script_path' is set.
         script_path: (optional) path to an AfterFX script to execute at startup.
+        script_args: (optional) dictionary of arguments to pass to the AfterFX script.
     """
 
-    def __init__(self, bin_path: str = None, no_gui: bool = False, script_path: str = None) -> None:
+    def __init__(
+        self, bin_path: str = None, no_gui: bool = False, script_path: str = None, script_args: Dict = None
+    ) -> None:
         super(Launcher, self).__init__()
 
-        self._cmd_builder = _CommandBuilder(bin_path=bin_path, no_gui=no_gui, script_path=script_path)
+        self._cmd_builder = _CommandBuilder(
+            bin_path=bin_path, no_gui=no_gui, script_path=script_path, script_args=script_args
+        )
 
     def _cmd_to_exec(self):
         return self._cmd_builder.build()
